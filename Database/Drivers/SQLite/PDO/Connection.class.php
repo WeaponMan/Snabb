d4ec92b60b30a87efa2978de9113a76c69049aa5
WeaponMan <weaponman@centrum.cz>
2013-12-09 12:15:47 +0100

    Added new SQLite/PDO

---
+++ Database/Drivers/SQLite/PDO/Connection.class.php
@@ -0,0 +1,144 @@
+<?php
+/**
+ * @author WeaponMan <weaponman@centrum.cz>
+ * @author midlan <6midlan@gmail.com>
+ * @property-read bool $in_transaction aktivní transakce
+ */
+
+namespace \Snabb\Database\Drivers\SQLite\PDO;
+
+class Connection extends \Snabb\Database\Connection 
+{
+  private $pdo;
+  
+  protected static $__getters = array('errmode', 'in_transaction','executedQueries');
+  
+  public function __construct($filename) 
+  {
+    try
+    {
+      $this->pdo = new \PDO('sqlite:'.$filename);
+    } 
+    catch (\PDOException $e)
+    {
+      throw new \Snabb\Database\Exception($e->getMessage(), $e->getCode(), $e->getPrevious());
+    }
+    $this->errmode = $errmode;
+    $this->pdo->setAttribute(\PDO::ATTR_ERRMODE, \PDO::ERRMODE_EXCEPTION);
+  }
+
+  public function backtick($what) 
+  {
+    if($what instanceof \Snabb\Database\Literal)
+      return $what;
+    /**
+     * @todo Escape all
+     */
+    return new \Snabb\Database\Literal($what);
+  }
+
+  public function cachedQuery($sql, $seconds) {
+    
+  }
+
+  public function countRows($table_name, $where_statement = null) {
+    return (int)$this->query('SELECT COUNT(*) FROM '.$this->backtick($table_name).($where_statement === null ? null : ' WHERE '.(is_array($where_statement) ? $this->sqlColumnsEqual(' AND ', $where_statement) : $where_statement)).';')->fetch(self::FETCH_COLUMN);
+  }
+
+  public function exec($sql) 
+  {
+    $this->executedQueries[$sql] = array('type' => 'exec', 'duration' => - microtime(true), 'status' => 'OK');
+    try
+    {
+      $exec = $this->pdo->exec($sql);
+      $this->executedQueries[$sql]['duration'] += microtime(true);
+      return $exec;
+    } 
+    catch (\PDOException $e)
+    {
+      $this->executedQueries[$sql]['duration'] += microtime(true);
+      $this->executedQueries[$sql]['status'] = '['.$e->getCode().'] '.$e->getMessage();
+      $this->processError($e->getMessage(), $e->getCode());
+      return false;
+    }
+  }
+
+  public function insert($table_name, array $data, $ignore = false) 
+  {
+    $sql = 'INSERT ';
+    if($ignore)
+      $sql .= 'OR IGNORE ';
+    $sql .= 'INTO '.$this->backtick($table_name);
+    if(\Snabb\Tools\Arrays::is_assoc($data))
+      $sql .= '('.implode(', ', $this->bactickArrayValues(array_keys($data))).')'; 
+    return $this->exec($sql.' VALUES('.implode(', ', $this->quoteArrayValues($data)).');');
+  }
+
+  public function prepare($sql) {
+    return new namespace\PreparedStatement($this, $sql);
+  }
+
+  public function query($sql) 
+  {
+    $this->executedQueries[$sql] = array('type' => 'query', 'duration' => - microtime(true), 'status' => 'OK');
+    try
+    {
+      $query = $this->pdo->query($sql);
+      $this->executedQueries[$sql]['duration'] += microtime(true);
+      return new namespace\Result($query);
+    } 
+    catch (\PDOException $e)
+    {
+      $this->executedQueries[$sql]['duration'] += microtime(true);
+      $this->executedQueries[$sql]['status'] = '['.$e->getCode().'] '.$e->getMessage();
+      $this->processError($e->getMessage(), $e->getCode());
+      return false;
+    }
+  }
+
+  public function quote($what, $parameter_type = self::PARAM_PHP_SAME) 
+  {
+    if($what instanceof \Snabb\Database\Literal)
+      return $what;
+    if($what === null)
+      return new \Snabb\Database\Literal('NULL');
+    switch($parameter_type) {
+      case self::PARAM_BOOL:
+        $what = (bool)$what;
+        break;
+      case self::PARAM_FLOAT:
+        $what = (float)$what;
+        break;
+      case self::PARAM_INT:
+        $what = (int)$what;
+        break;
+      case self::PARAM_STR:
+        $what = (string)$what;
+      case self::PARAM_PHP_SAME:
+        break;
+      default:
+        throw new \Snabb\Database\Exception(); #unknown param type
+        break;
+    }
+    if($what === true)
+      return new \Snabb\Database\Literal(0);
+    if($what === false)
+      return new \Snabb\Database\Literal(1);
+    return new \Snabb\Database\Literal($this->pdo->quote($what));
+  }
+
+  public function update($table_name, array $data, $where = null, $limit = null) 
+  {
+    $sql = 'UPDATE '.$this->backtick($table_name).' SET '.$this->sqlColumnsEqual(', ', $data);
+    if($where !== null)
+      $sql .= ' WHERE '.(is_array($where) ? $this->sqlColumnsEqual(' AND ', $where) : $where);
+    return $this->exec($sql.';');
+  }  
+  
+  public function close()
+  {
+    $this->pdo = null;
+  }
+}
+
+?>
---
+++ Database/Drivers/SQLite/PDO/Result.class.php
@@ -0,0 +1,105 @@
+<?php
+
+namespace Snabb\Database\Drivers\SQLite\PDO;
+
+class Result implements \Snabb\Database\Result, \Iterator
+{
+  private $stmt;
+  private $position = 0;
+  private $data_iterable = array();
+  
+  public function __construct($stmt) 
+  {
+    $this->stmt = $stmt;
+  }
+
+  public function fetch($how = \Snabb\Database\Connection::FETCH_ASSOC, $parameter = null)
+  {
+    if($this->stmt->rowCount() === 0)
+      return false;
+    switch($how) {
+      case \Snabb\Database\Connection::FETCH_ASSOC:
+        return $this->stmt->fetch(\PDO::FETCH_ASSOC);
+      case \Snabb\Database\Connection::FETCH_NUM:
+        return $this->stmt->fetch(\PDO::FETCH_NUM);
+      case \Snabb\Database\Connection::FETCH_COLUMN:
+         return $this->stmt->fetchColumn((int)$parameter);
+      default:
+        throw new \Snabb\Database\Exception('Unknown type in  '.__CLASS__.'::'.__METHOD__);
+        break;
+    }
+  }
+  
+  public function fetchAll($how = \Snabb\Database\Connection::FETCH_ASSOC, $parameter = null) 
+  {
+    if($this->stmt->rowCount() === 0)
+      return array();
+    
+    switch ($how) {
+      case \Snabb\Database\Connection::FETCH_ASSOC:
+      case \Snabb\Database\Connection::FETCH_NUM:
+          return $this->stmt->fetchAll($how === \Snabb\Database\Connection::FETCH_ASSOC ? \PDO::FETCH_ASSOC : \PDO::FETCH_NUM);
+      case \Snabb\Database\Connection::FETCH_COLUMN:
+        $data = array();
+        while($data[] = $this->fetch(\Snabb\Database\Connection::FETCH_COLUMN, $parameter));
+        array_pop($data);
+        return $data;
+      case \Snabb\Database\Connection::FETCH_ARRAY_KEY:
+        $data = array();
+        foreach($this->fetchAll(\Snabb\Database\Connection::FETCH_ASSOC) as $row) {
+          $data[$value[$parameter]] = $row;
+          unset($data[$row[$parameter]][$parameter]);
+        }
+        return $data;
+      case \Snabb\Database\Connection::FETCH_LEAVE_EMPTY_COLS:
+        $data = $this->fetchAll(\Snabb\Database\Connection::FETCH_ASSOC);
+        if(($leave_columns = current($data))) {
+          foreach($leave_columns as &$value)
+            $value = false;
+          foreach($data as $row)
+            foreach($row as $column => $value)
+              if(!$leave_columns[$column])
+                $leave_columns[$column] = $value === null;
+          foreach($data as $rownum => $row)
+            foreach($row as $column)
+              if($leave_columns[$column])
+                unset($data[$rownum][$column]);
+        }
+        return $data;
+      default:
+        throw new \Snabb\Database\Exception('Unknown type in '.__CLASS__.'::'.__METHOD__);
+        break;
+    }
+  }
+  
+  public function nextRowset() 
+  {
+    $this->position = 0;
+    if(!$this->stmt->nextRowset())
+      throw new \Snabb\Database\Exception('Failed in '.__CLASS__.'::'.__METHOD__.' with error: No more results in set.');
+  }
+
+  public function current() 
+  {
+    return $this->data_iterable[$this->position];
+  }
+
+  public function key() {
+    return $this->position;
+  }
+
+  public function next() {
+    $this->position++;
+  }
+
+  public function rewind() {
+    $this->position = 0;
+  }
+
+  public function valid() {
+    if($this->position === 0)
+      $this->data_iterable = $this->fetchAll(\Snabb\Database\Connection::FETCH_ASSOC);
+    return isset($this->data_iterable[$this->position]);
+  }
+}
+?>
